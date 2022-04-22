7b9e61317053095afcad564e7abf53bedf02bdbf
wrmf <31448920+wrmf@users.noreply.github.com>
2022-04-21 19:18:49 -0500

    Now showing and saving a map of the United States with cartopy!

--- CartopyTest.py
+++ CartopyTest.py
@@ -1,10 +1,25 @@
 import cartopy as ccrs
 import matplotlib.pyplot as plt
+import numpy as np
+import cartopy.io.shapereader as shpreader
 
-plt.figure(figsize=(9.4248, 3))
-ax = plt.axes(projection=ccrs.crs.LambertCylindrical())
-ax.stock_img()
+central_lat = 37.5
+central_lon = -96
+extent = [-120, -70, 24, 50.5]
+central_lon = np.mean(extent[:2])
+central_lat = np.mean(extent[2:])
 
+plt.figure(figsize=(12, 6))
+ax = plt.axes(projection=ccrs.crs.AlbersEqualArea(central_lon, central_lat))
+ax.set_extent(extent)
+
+ax.add_feature(ccrs.feature.OCEAN)
+ax.add_feature(ccrs.feature.LAND, edgecolor='black')
+ax.add_feature(ccrs.feature.LAKES, edgecolor='black')
+ax.add_feature(ccrs.feature.RIVERS)
+ax.add_feature(ccrs.feature.STATES)
+ax.gridlines()
+
+plt.savefig('testPlot.png')
 plt.show()
-plt.savefig('testPlot.png', transparent=True, dpi=1000)
 print('Saved plot')
